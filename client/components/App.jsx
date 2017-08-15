86adcf3dd6402fe9cb449eb2c64d52edddb2782a
harrison-symes <symeshjb@gmail.com>
2017-08-15 16:00:50 +1200

    single cat view is working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 import CatList from './CatList'
+import SingleCat from './SingleCat'
 
 const App = () => (
   <Router>
     <div className='app-container'>
       <h1>Hello Cats</h1>
       <Route exact path="/" component={CatList} />
+      <Route exact path='/cats/:id' component={(props) => <SingleCat {...props} />} />
     </div>
   </Router>
 )
